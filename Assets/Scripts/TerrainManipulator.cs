89a397f57ec6fa498b2a8e94f528e83f1befdf02
alainaolson21 <84754631+alainaolson21@users.noreply.github.com>
2023-06-07 03:56:36 -0700

    finished menu maybe, messing around with brushes
    nice

--- Assets/Scripts/TerrainManipulator.cs
+++ Assets/Scripts/TerrainManipulator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class TerrainManipulator : MonoBehaviour
 {
@@ -10,12 +11,74 @@ public class TerrainManipulator : MonoBehaviour
     [SerializeField] private float radius;
     [SerializeField] private float stddev;
     [SerializeField] private float n;
+    [SerializeField] private bool gaussian;
+    [SerializeField] private bool custom;
     [SerializeField] private bool addMode;
+    private TextMeshProUGUI _text;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        custom = false;
+        gaussian = true;
+        _text = GetComponent<TextMeshProUGUI>();
+        UpdateText();
         
+    }
+
+    public void NormalBrush() {
+        gaussian = false;
+        custom = false;
+    }
+
+    public void CustomBrush() {
+        custom = true;
+        gaussian = false;
+    }
+
+    public void UpdateText() 
+    {
+        _text.text = radius.ToString();
+    }
+
+    public void AddRadius() 
+    {
+        radius += 1f;
+        UpdateText();
+    }
+
+    public void SubtractRadius() 
+    {
+        radius -= 1f;
+        UpdateText();
+    }
+
+    public void LargeBrush() 
+    {
+        radius = 9f;
+        UpdateText();
+    }
+
+    public void SmallBrush() 
+    {
+        radius = 6f;
+        UpdateText();
+    }
+
+    public void AddTerrain() 
+    {
+        addMode = true;
+    }
+
+    public void RemoveTerrain() 
+    {
+        addMode = false;
+    }
+
+    public void GaussianMode()
+    {
+        gaussian = true;
+        custom = false;
     }
 
     // Update is called once per frame
@@ -38,7 +101,19 @@ public class TerrainManipulator : MonoBehaviour
                     for (int j = 0; j < intR; j++) {
                         // heights[i,j] += Mathf.Abs(transform.position.y - heights[i,j]) / TD.size.y;
                         int radJ = j - intR/2;
-                        float elevate = (1 / (Mathf.Pow(stddev, n) * Mathf.Pow(2*Mathf.PI, n/2))) * Mathf.Exp(-(radI*radI + radJ*radJ) / (2*stddev*stddev)) * Mathf.Abs(transform.position.y - heights[i,j]) / TD.size.y;
+                        float elevate = transform.position.y/TD.size.y; 
+                        if (gaussian) {
+                            elevate = (1 / (Mathf.Pow(stddev, n) * Mathf.Pow(2*Mathf.PI, n/2))) * Mathf.Exp(-(radI*radI + radJ*radJ) / (2*stddev*stddev)) * Mathf.Abs(transform.position.y - heights[i,j]) / TD.size.y;
+                        }
+                        if (custom) {
+                            // spikey thing?
+                            // elevate = transform.position.y*(intR/(i/2f+1f)*(j/2f+1f))/TD.size.y;
+                            if (radJ == 0 || radI == 0) {
+                                elevate = transform.position.y/TD.size.y;
+                            } else {
+                                elevate = transform.position.y*(intR/radJ)*(intR/radI)/TD.size.y;
+                            }
+                        }
                         // Debug.Log(elevate);
                         // Debug.Log(heights[i,j]);
                         
--- Assets/Scripts/UserTutorial.cs
+++ Assets/Scripts/UserTutorial.cs
@@ -5,53 +5,83 @@ using UnityEngine;
 public class UserTutorial : MonoBehaviour
 {
     public GameObject[] tutorialSteps;
+    public GameObject[] brushStyleScreenButton;
+    public GameObject[] mainMenuButton;
+    public GameObject[] tutorialMenuButton;
+    private bool brushStyleScreen;
+
+    private bool tutorial;
     private int currTutorialStep;
-    public GameObject exitButton;
-    public GameObject nextButton;
-    public GameObject restartButton;
-    public GameObject[] brushes;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < brushes.Length; i++) {
-            brushes[i].SetActive(true);
-        }
-        exitButton.SetActive(false);
-        nextButton.SetActive(false);
-        restartButton.SetActive(false);
+        brushStyleScreen = false;
+        tutorial = false;
+        
+        MainMenu();
     }
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < tutorialSteps.Length; i++)
-        {
-           if (i == currTutorialStep) {
-                tutorialSteps[currTutorialStep].SetActive(true);
-           } else {
-                tutorialSteps[i].SetActive(false);
-           }
+        if (tutorial) {
+            for (int i = 0; i < tutorialSteps.Length; i++)
+            {
+                if (i == currTutorialStep) {
+                        tutorialSteps[currTutorialStep].SetActive(true);
+                } else {
+                        tutorialSteps[i].SetActive(false);
+                }
+            }
         }
+    }
+
+    public void BrushStyleScreen(){
+            for (int i = 0; i < mainMenuButton.Length; i++) {
+            mainMenuButton[i].SetActive(false);
+        }
+
+        for (int i = 0; i < tutorialMenuButton.Length; i++) {
+            tutorialMenuButton[i].SetActive(false);
+        }
+
+        for (int i = 0; i < brushStyleScreenButton.Length; i++) {
+            brushStyleScreenButton[i].SetActive(true);
+        }
+    }
+
+    public void MainMenu() {
+        tutorial = false;
+        for (int i = 0; i < mainMenuButton.Length; i++) {
+            mainMenuButton[i].SetActive(true);
+        }
+
+        for (int i = 0; i < tutorialMenuButton.Length; i++) {
+            tutorialMenuButton[i].SetActive(false);
+        }
+        for (int i = 0; i < brushStyleScreenButton.Length; i++) {
+            brushStyleScreenButton[i].SetActive(false);
+        }
+
+        currTutorialStep = 0;
     }
 
     public void StartTutorial(){
-        for(int i = 0; i < brushes.Length; i++) {
-            brushes[i].SetActive(false);
+        tutorial = true;
+        for (int i = 0; i < mainMenuButton.Length; i++) {
+            mainMenuButton[i].SetActive(false);
         }
-        exitButton.SetActive(true);
-        nextButton.SetActive(true);
-        restartButton.SetActive(true);
+
+        for (int i = 0; i < tutorialMenuButton.Length; i++) {
+            tutorialMenuButton[i].SetActive(true);
+        }
     }
 
     public void EndTutorial() {
-        for(int i = 0; i < brushes.Length; i++) {
-            brushes[i].SetActive(true);
-        }
-        exitButton.SetActive(false);
-        nextButton.SetActive(false);
-        restartButton.SetActive(false);
-        currTutorialStep = 0;
+        MainMenu();
     }
 
     public void RestartTutorial() {
